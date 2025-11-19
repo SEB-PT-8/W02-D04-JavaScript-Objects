4ad0dc5a94e95dc66c6bbe8d6813768b4fd0f65f
omarakamal <omaraakamal@gmail.com>
2025-11-19 18:47:21 +0300

    update

--- app.js
+++ app.js
@@ -10,5 +10,50 @@ Data Types:
 7. Objects
 */
 
+// declaring a new student
+const myStudent = {
+    name: 'Khaled',
+    city: 'Manama',
+    phoneNumber: 59667941,
+    isStudent: true,
+    course:'SEB',
+}
 
-const myStudent = {}
+console.log(myStudent)
+
+console.log(myStudent.name)
+
+console.log(myStudent.city)
+
+
+console.log('I am ' + myStudent.name + ' and I am from ' + myStudent.city)
+
+
+// updating a key value pair (property)
+myStudent.city = 'Galali'
+
+console.log(myStudent)
+
+
+// Adding a new key value pair
+myStudent.email = 'khaled@gmail.com'
+
+console.log(myStudent)
+
+
+// deleting a key value pair
+delete myStudent.course
+console.log(myStudent)
+
+
+const allStudents = []
+
+allStudents.push(myStudent)
+
+
+const ahmedStudent = {
+    name:'ahmed',
+    isStudent:true,
+    
+}
+console.log(allStudents.length)
